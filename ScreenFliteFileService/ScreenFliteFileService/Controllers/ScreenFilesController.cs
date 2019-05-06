e88235ee63522409110835913e81ca0e0eb13074
Damien Tohin Doumer Kake <damientohin@gmail.com>
2019-05-06 13:57:27 +0200

    Finished basic API

--- ScreenFliteFileService/ScreenFliteFileService/Controllers/ScreenFilesController.cs
+++ ScreenFliteFileService/ScreenFliteFileService/Controllers/ScreenFilesController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,13 +14,13 @@ namespace ScreenFliteFileService.Controllers
     [ApiController]
     public class ScreenFilesController : ControllerBase
     {
-        string folder = "screenFlit";
+        string _folder = "screenFlit";
 
         [HttpGet("getFile/{fileName}")]
         public async Task<IActionResult> GetFile(string fileName)
         {
             await Task.CompletedTask;
-            string path = Path.Combine(GetFolderPath(SpecialFolder.Desktop), $"{folder}{Path.DirectorySeparatorChar}{fileName}");
+            string path = Path.Combine(GetFolderPath(SpecialFolder.Desktop), $"{_folder}{Path.DirectorySeparatorChar}{fileName}");
             if (!System.IO.File.Exists(path))
                 return NotFound();
             return File(System.IO.File.ReadAllBytes(path), "application/force-download", fileName);
@@ -28,10 +30,42 @@ namespace ScreenFliteFileService.Controllers
         public async Task<IActionResult> StreamVideo(string fileName)
         {
             await Task.CompletedTask;
-            string path = Path.Combine(GetFolderPath(SpecialFolder.Desktop), $"{folder}{Path.DirectorySeparatorChar}{fileName}");
+            string path = Path.Combine(GetFolderPath(SpecialFolder.Desktop), $"{_folder}{Path.DirectorySeparatorChar}{fileName}");
             if (!System.IO.File.Exists(path))
                 return NotFound();
             return File(System.IO.File.OpenRead(path), "video/mp4");
+        }
+
+        [HttpGet("getResource")]
+        public IActionResult GetResources()
+        {
+            return Ok(new { Resources = Directory.GetFiles(Path.Combine(GetFolderPath(SpecialFolder.Desktop), _folder)).Select(f => Path.GetFileName(f)) });
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> AddResource(IFormFile file)
+        {
+            try
+            {
+                // full path to file in temp location
+                //var filePath = Path.GetTempFileName();
+                string path = Path.Combine(GetFolderPath(SpecialFolder.Desktop), $"{_folder}{Path.DirectorySeparatorChar}{file.FileName}");
+
+                if (file == null || file.Length == 0)
+                    return Content("file not selected");
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                return Ok(new { FileName = path });
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                throw;
+            }
         }
     }
 }

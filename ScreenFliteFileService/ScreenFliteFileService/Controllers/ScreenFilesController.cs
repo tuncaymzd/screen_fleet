57b1d5a022c8c76e3d84957f97db664774b04eca
Damien Tohin Doumer Kake <damientohin@gmail.com>
2019-05-13 14:39:01 +0200

    I added comments

--- ScreenFliteFileService/ScreenFliteFileService/Controllers/ScreenFilesController.cs
+++ ScreenFliteFileService/ScreenFliteFileService/Controllers/ScreenFilesController.cs
@@ -10,12 +10,21 @@ using static System.Environment;
 
 namespace ScreenFliteFileService.Controllers
 {
+    /// <summary>
+    /// This Controller handles operations made on 
+    /// Resources at the Backend Level
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class ScreenFilesController : ControllerBase
     {
         string _folder = "screenFlit";
 
+        /// <summary>
+        /// Get a resource without streaming it.
+        /// </summary>
+        /// <param name="fileName">The name of the file to download.</param>
+        /// <returns></returns>
         [HttpGet("getFile/{fileName}")]
         public async Task<IActionResult> GetFile(string fileName)
         {
@@ -26,6 +35,11 @@ namespace ScreenFliteFileService.Controllers
             return File(System.IO.File.ReadAllBytes(path), "application/force-download", fileName);
         }
 
+        /// <summary>
+        /// Gets and Streams a video from the backend to the front end application requesting it.
+        /// </summary>
+        /// <param name="fileName">The name of the file to be streamed.</param>
+        /// <returns></returns>
         [HttpGet("streamVideo/{fileName}")]
         public async Task<IActionResult> StreamVideo(string fileName)
         {
@@ -36,12 +50,21 @@ namespace ScreenFliteFileService.Controllers
             return File(System.IO.File.OpenRead(path), "video/mp4");
         }
 
+        /// <summary>
+        /// Gets the list of resources available at the server level.
+        /// </summary>
+        /// <returns>List of regourses.</returns>
         [HttpGet("getResource")]
         public IActionResult GetResources()
         {
             return Ok(new { Resources = Directory.GetFiles(Path.Combine(GetFolderPath(SpecialFolder.Desktop), _folder)).Select(f => Path.GetFileName(f)) });
         }
 
+        /// <summary>
+        /// Uploads a new resource from the front end application to the backend server side.
+        /// </summary>
+        /// <param name="file">Name of the file to be uploaded.</param>
+        /// <returns>Confirmation of the upload process termination.</returns>
         [HttpPost("upload")]
         public async Task<IActionResult> AddResource(IFormFile file)
         {
--- ScreenFliteFileService/ScreenFliteFileService/Controllers/ValuesController.cs
+++ ScreenFliteFileService/ScreenFliteFileService/Controllers/ValuesController.cs
@@ -10,36 +10,17 @@ namespace ScreenFliteFileService.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            var resp = "API Up and Running perfectly !!!";
+            return Ok(resp);
         }
 
-        // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            return $"API Running perfectly. Gotten {id}";
         }
     }
 }
